99f19e82c438b991700fc9380b2992c0bb15e39e
tkc8441 <tkc8441@nyu.edu>
2025-11-27 15:22:07 -0500

    added simualator code and parsing logic

--- repcrec/src/main/java/com/ads/Simulator.java
+++ repcrec/src/main/java/com/ads/Simulator.java
@@ -1,5 +1,8 @@
 package com.ads;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import com.ads.interfaces.ITransactionManager;
 
 /**
@@ -10,12 +13,33 @@ import com.ads.interfaces.ITransactionManager;
 
 public class Simulator {
     private final ITransactionManager tm;
+    private final Scanner scanner;
+    private final CommandParser parser;
 
     public Simulator(ITransactionManager tm) {
         this.tm = tm;
+        scanner = new Scanner(System.in);
+        parser = new CommandParser();   
     }
 
     public static void main(String[] args) {
+        // Check if file input is provided otherwise take input from stdin
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-f") || args[i].equals("--file") && i + 1 < args.length) {
+                try {
+                    System.setIn(new java.io.FileInputStream(args[i + 1]));
+                } catch (FileNotFoundException e) {
+                    System.err.println("File not found: " + args[i + 1]);
+                    e.printStackTrace();
+                    return;
+                }
+                break;
+            }
+        }
+
+        final Simulator simulator = new Simulator(null);
+        simulator.run();
+        
         // TODO: Instantiate your concrete TransactionManager here once implemented
         // ITransactionManager tm = new TransactionManager(); 
         
@@ -26,7 +50,29 @@ public class Simulator {
         // driver.run(args);
     }
 
-    public void run(String[] args) {
-        //TODO: Implement reading from file or stdin
+    /**
+     * Runs the simulator, reading commands from input and executing them.
+     * @params none
+     * @return void
+     */
+    public void run() {
+        while (scanner.hasNextLine()) {
+            try {
+                String line = scanner.nextLine();
+                final Command command = parser.parse(line);
+                if (command != null) {
+                    // Call TM with this command
+                    System.out.println("Executing command: " + command);
+                } else {
+                    System.out.println("Invalid command: " + line);
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ignoring invalid argument: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("An unexpected error occurred: " + e.getMessage());
+                e.printStackTrace();
+                return;
+            }
+        }
     }
 }
